04a62cab453f78b3a637f90ceb99cded5b0778c9
noor shah <noor.shah@virginholidays.com>
2015-04-07 11:21:53 +0100

    Creating repo for rest assured api testing

---
+++ src/test/java/RestTest.java
@@ -0,0 +1,24 @@
+import static com.jayway.restassured.RestAssured.get;
+import org.json.JSONArray;
+import org.json.JSONException;
+import java.lang.String;
+import org.junit.Test;
+import org.junit.Assert;
+
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.RestAssured.*;
+import com.jayway.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+import com.jayway.restassured.module.jsv.JsonSchemaValidator.*;
+public class RestTest {
+    @Test
+    public void getRequestFindCapital() {
+    //make get request to fetch capital of norway
+    Response resp = get("http://restcountries.eu/rest/v1/name/norway");
+        get("http://restcountries.eu/rest/v1/name/norway").then().body("name", hasItem("Norway"));
+        get("http://restcountries.eu/rest/v1/name/norway").then().body("capital", hasItem("Oslo"));
+    }
+
+}
+
+
